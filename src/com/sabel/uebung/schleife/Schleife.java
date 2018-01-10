6cfa8b018266b1d6f1e1cef95ef625263a0c3188
Maximilian Reithmayer <m.reithmayer@gmx.net>
2018-01-10 12:38:35 +0100

    Janka added

--- src/com/sabel/uebung/schleife/Schleife.java
+++ src/com/sabel/uebung/schleife/Schleife.java
@@ -1,48 +1,72 @@
-package com.sabel.uebung.schleife;
+package com.sabel.uebung.schleifen;
 
-public class Schleife {
-    public int summieren(int a, int b){
+public class Schleifen {
+    //Aufgabe 4: Summieren von allen Werten zwischen a und b, inklusive a und b
+    //Klassenmethode (statische Methode), nicht an ein Obejekt gebunden. Zusatz "static"
+    //Aufruf: Klassenname.Methodenname
+    public static int summieren(int a, int b) {
         int zaehler = a;
         int summe = 0;
-        while(zaehler <= b){
+        while (zaehler <= b) {
             summe += zaehler;
             zaehler++;
         }
         return summe;
     }
 
+    // Aufgabe 5: Erkenne ob Wert n eine Primzahl ist
+    public static boolean isPrim(int n) {
+        int teiler = 2;
+        while (teiler < n) {
+            if (n % teiler == 0) {  // teilt der Teiler die Zahl n ohne Rest, wenn ja, keine Primzahl
+                return false;
+            }
+        }
+        // Wenn alle Teiler "getestet", dann ist n eine Primzahl
+        return true;
+    }
+
     public static void main(String[] args) {
-        Schleife schleifen = new Schleife();
-        int summe = schleifen.summieren(1, 10);
-        System.out.println(summe);
+
+        // Methode summieren (Aufgabe 4) aufrufen
+        int summe = summieren(1, 10);
+        System.out.println(summe + "\n");
+
+        // Methode isPrim (Aufgabe 5) aufrufen. Ist 127 eine Primzahl.
+        if (isPrim(127)) {
+            System.out.println("Ist Primzahl");
+        } else {
+            System.out.println("Ist keine Primzahl");
+        }
+
+
+        //Aufgabe 1: Ausgabe der Zahlen von 0 bis 4
         int zahl = 0;
-        while (zahl < 5){
+        while (zahl < 5) {
             System.out.println(zahl);
             zahl++;
         }
         System.out.println("Wert von zahl nach der Schleife: " + zahl);
 
-        //Aufgabe 2: Ausgabe aller vielfachen von 5 von 20 bis 100,
-        int count = 0;
+        // Aufgabe 2: Ausgabe aller Vielfachen von Fünf von 20 bis 100 jeweils einschließlich
         zahl = 20;
-        while(zahl < 101){
+        int count = 0;
+        while (zahl < 101) {
             System.out.println(zahl);
             zahl += 5;
             count++;
         }
-
         System.out.println("Die Schleife wurde " + count + " mal durchlaufen");
 
-        // Aufgabe 3: Alle ungeraden zahlen zwischen 3 und 250
-
+        // Aufgabe 3: Ausgabe aller ungeraden Zahlen zwischen 3 und 250
         zahl = 3;
-        while(zahl < 250){
-            if (zahl % 2 == 1){
+        while (zahl < 250) {
+            if (zahl % 2 != 0) {
                 System.out.println(zahl);
             }
             zahl++;
         }
-
-
     }
 }
+
+
